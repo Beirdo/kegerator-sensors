21266453026243e9e5adefcc706fdb7214319c17
Gavin Hurlbut <gjhurlbu@gmail.com>
2011-03-19 19:14:50 -0700

    Reorganizing to redo sensors as modules

---
+++ old/temperature-sensor/schematic/old/tempsensor.cmd
@@ -0,0 +1,59 @@
+# Pin name action command file
+
+# Start of element CONN1
+ChangePinName(CONN1, 7, 7)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 8, 8)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element U2
+ChangePinName(U2, 6, VoutA)
+ChangePinName(U2, 5, Vin)
+ChangePinName(U2, 4, VoutB)
+ChangePinName(U2, 3, EnableB)
+ChangePinName(U2, 2, GND)
+ChangePinName(U2, 1, EnableA)
+
+# Start of element U1
+ChangePinName(U1, 8, VCC)
+ChangePinName(U1, 7, DO+/RI+)
+ChangePinName(U1, 6, DO-/RI-)
+ChangePinName(U1, 5, RE)
+ChangePinName(U1, 4, GND)
+ChangePinName(U1, 3, Rout)
+ChangePinName(U1, 2, Din)
+ChangePinName(U1, 1, DE)
+
+# Start of element U4
+ChangePinName(U4, 8, 8)
+ChangePinName(U4, 7, 7)
+ChangePinName(U4, 6, 6)
+ChangePinName(U4, 5, 5)
+ChangePinName(U4, 4, 4)
+ChangePinName(U4, 3, 3)
+ChangePinName(U4, 2, 2)
+ChangePinName(U4, 1, 1)
+
+# Start of element U3
+ChangePinName(U3, 14, GND)
+ChangePinName(U3, 13, PA0)
+ChangePinName(U3, 12, PA1)
+ChangePinName(U3, 11, PA2)
+ChangePinName(U3, 10, PA3)
+ChangePinName(U3, 9, PA4)
+ChangePinName(U3, 8, PA5)
+ChangePinName(U3, 7, PA6)
+ChangePinName(U3, 6, PA5)
+ChangePinName(U3, 5, "PB2 (INT0)")
+ChangePinName(U3, 4, PB3)
+ChangePinName(U3, 3, PB1)
+ChangePinName(U3, 2, PB0)
+ChangePinName(U3, 1, VCC)
